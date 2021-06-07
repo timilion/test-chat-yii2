a0708d5da6e21b4368a6c1117aa62af703fbfb8f
Tim <timilon10271@gmail.com>
2021-06-07 13:58:00 +0500

    fix xss

--- views/site/index.php
+++ views/site/index.php
@@ -4,6 +4,7 @@
 
 /** @var \app\models\ChatMessage $model */
 
+use yii\helpers\Html;
 use yii\helpers\Url;
 
 $this->title = 'Чат';
@@ -19,7 +20,7 @@ $isAdm = Yii::$app->user->can(\app\models\User::ROLE_ADMIN);
             <div class="direct-chat-messages" style="height: 450px;">
                 <!-- Message. Default to the left -->
                 <?php foreach ($model as $item): ?>
-                    <?php if ($item->isStatus() || $isAdm): ?>
+        
                         <div class="direct-chat-msg" <?= !$item->isStatus() ? 'style="opacity: 0.7;"' : '' ?>>
                             <div class="direct-chat-infos clearfix">
                                 <span class="direct-chat-name float-left"><?= $item->user->login ?></span>
@@ -31,7 +32,7 @@ $isAdm = Yii::$app->user->can(\app\models\User::ROLE_ADMIN);
                             <img class="direct-chat-img" src="/no-img.png" alt="Message User Image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text d-flex justify-content-between align-content-center <?= $item->isAdmin() ? 'adm' : '' ?>">
-                                <span><?= $item->text ?></span>
+                                <span><?= Html::encode($item->text) ?></span>
                                 <?php if ($isAdm && $item->isStatus()): ?>
                                     <a href="<?= Url::to(['site/incorrect', 'id' => $item->id]) ?>" class="trash">
                                         <i class="fas fa-minus-circle"></i>
@@ -40,7 +41,6 @@ $isAdm = Yii::$app->user->can(\app\models\User::ROLE_ADMIN);
                             </div>
                             <!-- /.direct-chat-text -->
                         </div>
-                    <?php endif; ?>
                 <?php endforeach; ?>
                 <!-- /.direct-chat-msg -->
             </div>
